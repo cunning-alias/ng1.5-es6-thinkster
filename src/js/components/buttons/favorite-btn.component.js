1d83060799b12ed305c54f4a1ff74ac22712668f
cunning-alias <dkweb101@yahoo.com>
2016-12-08 11:15:39 +0000

    fav button

--- src/js/components/buttons/favorite-btn.component.js
+++ src/js/components/buttons/favorite-btn.component.js
@@ -1,8 +1,40 @@
 class FavoriteBtnCtrl {
-	constructor() {
+	constructor(User, Articles, $state) {
 		'ngInject';
 
+		this._Articles = Articles;
+		this._User = User;
+		this._$state = $state;
+	}
 
+	submit() {
+		this.isSubmitting = true;
+
+		if (!this._User.current) {
+			this._$state.go('app.register');
+			return;
+		}
+
+		// if fav'd already, unfav it
+		if (this.article.favorited) {
+			this._Articles.unfavorite(this.article.slug).then(
+				() => {
+					this.isSubmitting = false;
+					this.article.favorited = false;
+					this.article.favoritesCount--;
+				}
+			)
+		
+		// otherwise, fav it
+		} else {
+			this._Articles.favorite(this.article.slug).then(
+				() => {
+					this.isSubmitting = false;
+					this.article.favorited = true;
+					this.article.favoritesCount++;
+				}
+			)
+		}
 	}
 }
 
