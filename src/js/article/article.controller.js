dc44134dd8acff9991386d829b735de9d92b1dc9
cunning-alias <dkweb101@yahoo.com>
2016-12-08 11:41:40 +0000

    comments

--- src/js/article/article.controller.js
+++ src/js/article/article.controller.js
@@ -1,10 +1,13 @@
 import marked from 'marked';
 
 class ArticleCtrl {
-  constructor(article, $sce, $rootScope) {
+  constructor(article, User, Comments, $sce, $rootScope) {
     'ngInject';
 
     this.article = article;
+    this._Comments = Comments;
+
+    this.currentUser = User.current;
 
     // update the title of the page
     $rootScope.setPageTitle(this.article.title);
@@ -12,6 +15,32 @@ class ArticleCtrl {
     // transform the markdown into HTML
     this.article.body = $sce.trustAsHtml(marked(this.article.body, {sanitize: true}));
 
+    // Initialize blank comment form
+    this.resetCommentForm();
+
+  }
+
+  resetCommentForm() {
+  	this.commentForm = {
+  		isSubmitting: false,
+  		body: '',
+  		errors: []
+  	}
+  }
+
+  addComment() {
+  	this.commentForm.isSubmitting = true;
+
+  	this._Comments.add(this.article.slug, this.commentForm.body).then(
+  		(comment) => {
+  			console.log(comment);
+  			this.resetCommentForm();
+  		},
+  		(err) => {
+  			this.commentForm.isSubmitting = false;
+  			this.commentForm.errors = err.data.errors;
+  		}
+  	);
   }
 }
 
